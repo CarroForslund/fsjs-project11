2a6fc5f47a306ac9a4ad22bc1276d3e01dc32e90
Caroline Forslund <cf222ev@student.lnu.se>
2020-05-17 21:05:21 +0100

    middleware updated for DRY reasons

--- routes/courses.js
+++ routes/courses.js
@@ -2,72 +2,8 @@ var express = require('express');
 var router = express.Router();
 const db = require('../db');
 const { Course, User } = db.models;
-const bcryptjs = require('bcryptjs');
-const auth = require('basic-auth');
-
-
-// Handler function to wrap each route
-function asyncHandler(cb){
-  return async(req, res, next) => {
-      try{
-          await cb(req, res, next)
-      } catch(error) {
-          next(error);
-      }
-  }
-}
-
-const authenticateUser = async(req, res, next) => {
-  let message = null;
-  // Parse the user's credentials from the Authorization header.
-  const credentials = auth(req);
-
-  // If the user's credentials are available...
-  if (credentials) {
-    // Attempt to retrieve the user from the data store
-    // by their username (i.e. the user's "key"
-    // from the Authorization header).
-    // const user = users.find(u => u.username === credentials.name);
-    
-    const user = await User.findOne({
-      where: {
-        emailAddress: credentials.name,
-      }
-    });
-
-    // If a user was successfully retrieved from the data store...
-    if (user) {
-      // Use the bcryptjs npm package to compare the user's password
-      // (from the Authorization header) to the user's password
-      // that was retrieved from the data store.
-      const authenticated = bcryptjs
-        .compareSync(credentials.pass, user.password);
-      // If the passwords match...
-      if (authenticated) {
-        // Then store the retrieved user object on the request object
-        // so any middleware functions that follow this middleware function
-        // will have access to the user's information.
-        req.currentUser = user;
-      } else {
-        message = `Authentication failure for username: ${user.username}`;
-      }
-    } else {
-      message = `User not found for username: ${credentials.name}`;
-    }
-  } else {
-    message = 'Auth header not found';
-  }
-  // If user authentication failed...
-  if (message) {
-    console.warn(message);
-    // Return a response with a 401 Unauthorized HTTP status code.
-    res.status(401).json({ message: 'Access Denied' });
-  } else {
-    // Or if user authentication succeeded...
-    // Call the next() method.
-    next();
-  }
-};
+const authenticateUser = require('../middleware/authenticate-user');
+const asyncHandler = require('../middleware/async-handler');
 
 // GET /api/courses
 // 200 - Returns a list of courses (including the user that owns each course)
@@ -122,7 +58,7 @@ router.get('/:id', asyncHandler(async (req, res, next) => {
     }
   });
   if(course) {
-    res.status(200).json();
+    res.status(200).json(course);
   } else {
     res.status(404).json({message: 'Course not found'});
   }
